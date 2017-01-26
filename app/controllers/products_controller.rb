3cd0c08fafeae04d35a23580eddff72d12a30f5d
Alphonse Habyarimana <habyara.malpha@gmail.com>
2017-01-26 09:54:30 +0200

    Redisigned Electronics Store App

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,18 @@
 class ProductsController < ApplicationController
   def index
-    @etl_items = Product.all
+    @products = Product.all
     render "index.html.erb"
   end
 
   def show
-    @etl_item = Product.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
   end
 
   def new
   end
 
   def create
-    etl_item = Product.new({
+    product = Product.new({
     name: params[:name],
     item_type: params[:item_type],
     make: params[:make],
@@ -21,18 +21,18 @@ class ProductsController < ApplicationController
     price: params[:price],
     shipping_period: params[:shipping_period]
     })
-    etl_item.save
+    product.save
     flash[:success] = "New Item Created"
-    redirect_to "/products/#{etl_item.id}"
+    redirect_to "/products/#{product.id}"
   end
 
   def edit
-    @etl_item = Product.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
   end
 
   def update
-    etl_item = Product.find_by(id: params[:id])
-    etl_item.assign_attributes({
+    product = Product.find_by(id: params[:id])
+    product.assign_attributes({
     name: params[:name],
     item_type: params[:item_type],
     make: params[:make],
@@ -41,14 +41,14 @@ class ProductsController < ApplicationController
     price: params[:price],
     shipping_period: params[:shipping_period]
     })
-    etl_item.save
+    product.save
     flash[:success] = "Item Updated"
-    redirect_to "/products/#{etl_item.id}"
+    redirect_to "/products"
   end
 
   def destroy
-    @etl_item = Product.find_by(id: params[:id])
-    @etl_item.destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
     flash[:success] = "Item Deleted!"
     redirect_to "/products"
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+
+  def friendly_created_at
+    created_at.strftime("%H:%M on %d-%m-%Y")
+  end
+
+  def friendly_updated_at
+    updated_at.strftime("%H:%M on %d-%m-%Y")
+  end
+
+  def tax
+    price*0.09
+  end 
+
 end
